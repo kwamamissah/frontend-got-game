f1670e21c53b7974a774c6e8f24dc70efbfc5568
github username <github email address>
2019-01-18 17:13:21 -0500

    frontend works! now who GOTgame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+const API = `http://localhost:3000/characters`
 class App extends Component {
+
+  state = {
+    characters: []
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(json => this.setState({ characters: json }))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.characters.map(character => <h4>{character.name}</h4>)}
       </div>
     );
   }
