2fc19919704683656f7559fbfe5681e503c26d9b
github username <github email address>
2019-01-24 10:46:59 -0500

    preview video autoplays

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -7,7 +7,7 @@ const GameOver = (props) => {
   return(
     <Container textAlign='center' style={{"margin-top": "40px"}}>
       <div> Game Over </div>
-      <iframe title='GOT' width="560" height="315" src="https://www.youtube.com/embed/0MGnIUn30FU"
+      <iframe title='GOT' width="560" height="315" src="https://www.youtube.com/embed/0MGnIUn30FU?&autoplay=1"
       frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen></iframe>
       <br />
