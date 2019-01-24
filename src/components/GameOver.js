b665eccfde1f362c4fafef0aba5623563e1f36f5
github username <github email address>
2019-01-24 10:44:08 -0500

    added game of throne szn 8 preview video to Game Over

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-import { Button } from 'semantic-ui-react'
+import { Button, Container } from 'semantic-ui-react'
 
 const GameOver = (props) => {
 
   return(
-    <div>
+    <Container textAlign='center' style={{"margin-top": "40px"}}>
       <div> Game Over </div>
+      <iframe title='GOT' width="560" height="315" src="https://www.youtube.com/embed/0MGnIUn30FU"
+      frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen></iframe>
+      <br />
       <Button onClick={() => props.restartGame()}> Restart Game </Button>
-    </div>
+    </Container>
 
 
   )
