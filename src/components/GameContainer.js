a324492c32a05588099521809cceab63072a59ca
github username <github email address>
2019-01-24 20:52:19 -0500

    done styling

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 import CharacterContainer from './CharacterContainer.js'
+import { Segment } from 'semantic-ui-react'
+
+
+let image = `https://media.giphy.com/media/d97OPMEimWGBi/giphy.gif`
+const mainBg = {
+  background: `url(${image})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  height: '900px'
+}
+
 
 
 export default class GameContainer extends Component {
 
   render() {
     return(
-      <div>
+      <Segment style= { mainBg }>
         <CharacterContainer />
-      </div>
+      </Segment>
     )
   }
 }
